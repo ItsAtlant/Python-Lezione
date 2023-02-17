f3fce2dd1192ca075d7b5047eee064422a24572c
Davide Soltys <Davide02072003@gmail.com>
2023-02-17 09:39:40 +0100

    Create esercizio1.py

---
+++ funzioni/esercizio1.py
@@ -0,0 +1,22 @@
+#if the user write and the leght of the text is even write it in a file even if its odd write it in a file odd
+print("Per uscire premi due volte invio")
+
+while True:
+    nome = input("Inserire il testo: ")
+    if nome == "  ":
+        break
+    lenght = len(nome)
+    def primafunzione():
+        def parifunzione():
+                f = open("even.txt","w")
+                f.write(nome)
+                print("pari")
+        def disparifunzione():
+                f = open("odd.txt","w")
+                f.write(nome)
+                print("dispari")
+        if lenght%2 == 0:
+            parifunzione()
+        else:
+            disparifunzione()
+    primafunzione()
