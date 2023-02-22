43a4028a53f5b48abf4b7b55b531b7d11d083399
Davide Soltys <Davide02072003@gmail.com>
2023-02-22 19:35:36 +0100

    Manca da fare i TODO, file perfezionato e funziona perfettamente

--- compiti_22Febbrary2023/codice.py
+++ compiti_22Febbrary2023/codice.py
@@ -1,13 +1,28 @@
-
+# TODO:far mettere il path del salvataggio del report 
+# TODO:Scrivere una funziona da riga 19 a 24
+# TODO:salvare il report in un altro file
 import requests
 
 image_url = input("Inserisci il link: ")
   
-# URL of the image to be downloaded is defined as image_url
-r = requests.get(image_url) # create HTTP response object
-  
-# send a HTTP request to the server and save
-# the HTTP response in a response object called r
-with open("C:\Users\DavideSoltys\Desktop\il mio github\Python-Lezione\compiti_22FEBBRARY2023\ChosenSite.html",'w') as f:
 
-    f.write(r.content)
+r = requests.get(image_url) 
+  
+with open(r"C:\Users\DavideSoltys\Desktop\il mio github\Python-Lezione\compiti_22FEBBRARY2023\ChosenSite.html",'w') as f:
+
+    f.write(r.text)
+
+i,k,c = 0,0,0
+
+with open ("ChosenSite.html") as f:
+    file = f.read()
+    for x in range (len(file)):
+        if "img " == file[x:x+4]:
+            i = i + 1
+        if "class=" == file[x:x+6]:
+            k = k + 1
+        if "https" == file[x:x+5]:
+            c = c + 1
+
+print("Nel file ci sono:")
+print(i,"immagini",k,"classi",c,"link") 
--- compiti_22Febbrary2023/prove.py
+++ compiti_22Febbrary2023/prove.py
@@ -1,30 +1,29 @@
 
-# imported the requests library
 import requests
 
 image_url = input("Inserisci il link: ")
   
-# URL of the image to be downloaded is defined as image_url
-r = requests.get(image_url) # create HTTP response object
+
+r = requests.get(image_url) 
   
-# send a HTTP request to the server and save
-# the HTTP response in a response object called r
 with open(r"C:\Users\DavideSoltys\Desktop\il mio github\Python-Lezione\compiti_22FEBBRARY2023\ChosenSite.html",'wb') as f:
-  
-    # Saving received content as a png file in
-    # binary format
-  
-    # write the contents of the response (r.content)
-    # to a new file in binary mode.
+
     f.write(r.content)
 
 i = 0
+k = 0 
+c = 0
 with open ("ChosenSite.html", encoding="utf8") as f:
     file = f.read()
     try:
         for x in range (len(file)):
             if "img " == file[x] + file[x+1] + file[x+2] + file[x+3]:
                 i = i + 1
+            if "class=" == file[x] + file[x+1] + file[x+2] + file[x+3] + file[x+4] + file[x+5]:
+                k = k + 1
+            if "https" == file[x] + file[x+1] + file[x+2] + file[x+3] + file[x+4]:
+                c = c + 1
     except:
         print("")
-print("Nel file c'è scritto ",i," volte img")
+print("Nel file ci sono:")
+print(i,"immagini",k,"classi",c,"link") 
