a4baba658eb7d3dafaf61d181ee5abfcc5393211
Davide Soltys <Davide02072003@gmail.com>
2023-03-01 16:41:45 +0100

    Upgrade 22Febbrary
    Ora riesce a prendere il path e scrivere in un file

--- compiti_22Febbrary2023/codice.py
+++ compiti_22Febbrary2023/codice.py
@@ -3,18 +3,19 @@
 # TODO:Salvare il report in un altro file
 import requests
 
-image_url = input("Inserisci il link: ")
-  
+link = input("Inserisci il link: ")
+site = requests.get(link) 
 
-site = requests.get(image_url) 
-  
-with open(r"C:\Users\DavideSoltys\Desktop\il mio github\Python-Lezione\compiti_22FEBBRARY2023\ChosenSite.html",'w') as f:
+path = input("Iserisci il path per salvare il SITO: ") 
+Reportpath = input("Inserisci il path per salvare il REPORT: ")
+
+with open(path,'w') as f:
 
     f.write(site.text)
 
 i,k,c = 0,0,0
 
-with open (r"C:\Users\DavideSoltys\Desktop\il mio github\Python-Lezione\compiti_22FEBBRARY2023\ChosenSite.html") as f:
+with open (path,"r") as f, open(Reportpath,"w") as f2:
     file = f.read()
     for x in range (len(file)):
         if "img " == file[x:x+4]:
@@ -23,6 +24,9 @@ with open (r"C:\Users\DavideSoltys\Desktop\il mio github\Python-Lezione\compiti_
             k = k + 1
         if "href" == file[x:x+4]:
             c = c + 1
-
+    f2.write("Nel file ci sono:\n")
+    f2.write(f"{i} immagini\n")
+    f2.write(f"{k} classi\n")
+    f2.write(f"{c} siti\n")
 print("Nel file ci sono:")
 print(i,"immagini",k,"classi",c,"link") 
